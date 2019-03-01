c5cdfbb1bded2bd98d9b5daa9b594483cb14bc79
QiuRRQ <azizurrohim13@gmail.com>
2019-03-01 20:17:00 +0700

    add listing openshift IP

--- domain.js
+++ domain.js
@@ -4,6 +4,9 @@ const express = require('express');
 
 var app = express();
 
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
 app.get('/', function(req, res, next){
 
 	let scrape = async () => {
@@ -48,6 +51,8 @@ app.get('/', function(req, res, next){
 	});
 
 	
-})
+});
 //test
-app.listen(8080);
+app.listen(server_port, server_ip_address, function () {
+	 console.log( "Listening on " + server_ip_address + ", server_port " + server_port );
+});
