d1b5591c4d7c6d0378c0480c7e04185b15bfddc5
QiuRRQ <azizurrohim13@gmail.com>
2019-03-02 11:28:28 +0700

    no message

--- domain.js
+++ domain.js
@@ -4,8 +4,8 @@ const express = require('express');
 
 var app = express();
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var server_port = 8080;
+var server_ip_address = 172.30.141.14;
 
 app.get('/', function(req, res, next){
 
